50c216b79bd20ec8ea2d1628bdc0cf9bac728af9
Michael Paris <mlparis92@gmail.com>
2015-01-17 17:13:23 -0500

    user update

--- api/views.py
+++ api/views.py
@@ -17,12 +17,12 @@ class UserViewSet(viewsets.ModelViewSet):
 
     def create(self, request):
         data = request.data
-        user_serializer = ProfileSerializer(data=data)
+        user_serializer = ProfileSerializer(data=data, context={'request': request})
         if user_serializer.is_valid():
             new_user = user_serializer.save()
             return Response(user_serializer.data, status=status.HTTP_201_CREATED)
         else:
-            return Response({"error": "New User could not be created with given data"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"error": user_serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
     # def list(self, request):
     #     pass
