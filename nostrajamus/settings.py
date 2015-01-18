8b1075e2b054d471a328b0bb722c9bd8dc95d716
Michael Paris <mlparis92@gmail.com>
2015-01-17 20:07:47 -0500

    update

--- nostrajamus/settings.py
+++ nostrajamus/settings.py
@@ -77,7 +77,7 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+        'rest_framework.permissions.IsAuthenticated',
         'rest_framework.permissions.IsAdminUser'
     ],
     'DEFAULT_RENDERER_CLASSES': (
