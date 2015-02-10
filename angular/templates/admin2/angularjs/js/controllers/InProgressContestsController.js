6820fb48bfbee4688c711d3e1a40b78a4f52ddc8
Michael Paris <mlparis92@gmail.com>
2015-02-10 00:01:40 -0500

    Fixed Contests added details button. New background

--- angular/templates/admin2/angularjs/js/controllers/InProgressContestsController.js
+++ angular/templates/admin2/angularjs/js/controllers/InProgressContestsController.js
@@ -26,6 +26,28 @@ MetronicApp.controller('InProgressContestsController', ['$rootScope', '$scope', 
     $scope.contestInfo.start_time = $scope.timeOffset($scope.contestInfo.start_time, '+0');
     $scope.contestInfo.end_time = $scope.timeOffset($scope.contestInfo.end_time, '+0');
 
+    var insertCommas = function(s) {
+        // get stuff before the dot
+        var d = s.indexOf('.');
+        var s2 = d === -1 ? s : s.slice(0, d);
+        // insert commas every 3 digits from the right
+        for (var i = s2.length - 3; i > 0; i -= 3)
+          s2 = s2.slice(0, i) + ',' + s2.slice(i);
+        // append fractional part
+        if (d !== -1)
+          s2 += s.slice(d);
+        return s2;
+    }
+
+    $scope.modalEntry = {};
+    $scope.updateModal = function(entry) {
+        entry.percentage_increase = "+ " + insertCommas((((entry.current_playback_count - entry.initial_playback_count)/entry.initial_playback_count)*100).toFixed(2).toString()) + "%";
+        entry.initial_playback_count = insertCommas(entry.initial_playback_count.toString());
+        entry.current_playback_count = insertCommas(entry.current_playback_count.toString());
+        entry.current_follower_count = insertCommas(entry.current_follower_count.toString());
+        $scope.modalEntry = entry;
+    };
+
 
     // Start Countdown Timer
     // set the date we're counting down to
